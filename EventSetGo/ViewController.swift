94ccddd1d9672fdc83593f062904083d3d140b33
Ivor Addo <idehao@gmail.com>
2018-01-11 13:49:12 -0600

    Initial Commit

---
+++ EventSetGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EventSetGo
+//
+//  Created by Ivor D. Addo on 1/11/18.
+//  Copyright © 2018 Event Set Go. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
